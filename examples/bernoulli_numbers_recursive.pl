3ea71efd6c822b86b13bc4e2efcaa27f8594612c
trizen <trizenx@gmail.com>
2016-10-04 22:20:31 +0300

    modified:   examples/bernoulli_numbers_recursive.pl -- minor improvements

--- examples/bernoulli_numbers_recursive.pl
+++ examples/bernoulli_numbers_recursive.pl
@@ -6,9 +6,7 @@
 # Website: https://github.com/trizen
 
 # Recursive computation of Bernoulli numbers.
-
-# See: https://en.wikipedia.org/wiki/Bernoulli_number#Recursive_definition
-#      https://en.wikipedia.org/wiki/Binomial_coefficient#Recursive_formula
+# https://en.wikipedia.org/wiki/Bernoulli_number#Recursive_definition
 
 use 5.010;
 use strict;
@@ -16,29 +14,24 @@ use warnings;
 
 use lib qw(../lib);
 use Memoize qw(memoize);
-use Math::BigNum qw(:constant);
+use Math::BigNum qw(:constant binomial);
 
 memoize('bernoulli');
-memoize('binomial');
-
-sub binomial {
-    my ($n, $k) = @_;
-    $k == 0 || $n == $k ? 1 : binomial($n - 1, $k - 1) + binomial($n - 1, $k);
-}
 
 sub bernoulli {
     my ($n) = @_;
 
-    return 0.5 if $n == 1;
-    return 0.0 if $n % 2;
+    return 0.5 if $n == "1";
+    return 0.0 if $n %  "2";
+    return 1.0 if $n == "0";
 
-    my $bern = 1;
-    foreach my $k (0 .. $n - 1) {
-        $bern -= bernoulli($k) * binomial($n, $k) / ($n - $k + 1);
+    my $bern = 0.5 - 1/($n+1);
+    for (my $k = "2" ; $k < $n ; $k += "2") {
+        $bern -= bernoulli($k) * binomial($n, $k) / ($n - $k + "1");
     }
     $bern;
 }
 
-for (my $i = 0 ; $i <= 100 ; $i += 2) {
-    printf "B%-2d = %s\n", $i, bernoulli($i)->as_rat;
+foreach my $i (0 .. 50) {
+    printf "B%-3d = %s\n", "2"*$i, bernoulli("2"*$i)->as_rat;
 }
